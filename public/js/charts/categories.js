0615466d1492a7c88390be411c8dcb4c34c2eef6
nAndy <nandy@wikia-inc.com>
2014-03-15 19:25:07 +0100

    added legend for pie chart

--- public/js/charts/categories.js
+++ public/js/charts/categories.js
@@ -1,20 +1,27 @@
 $(function() {
 	// mocked data
+	var categoriesNames = [ "Holes", "Signs broken", "Signs missing", "Garbage" ];
 	var categoriesColors = [ "#FF0000", "#E0E4CC", "#69D2E7", "#F38630" ];
 	var categoryReports = [ 40, 15, 5, 22 ];
 
 	var pieCtx = $( "#categoriesPie" ).get(0).getContext("2d");
 	var data = [];
 	var l = categoryReports.length;
+	var rowTpl = '<tr><td><span style="background-color: {{color}};"></span></td><td>{{name}}</td></tr>';
+	var html = '';
 	for( var i = 0; i < l; i++ ) {
+		var color = categoriesColors[i];
 		var el = {
 			value: categoryReports[i],
-			color: categoriesColors[i]
+			color: color
 		};
 
+		html += Mustache.render( rowTpl, { 'color': color, name: categoriesNames[i] } );
 		data.push(el);
 	}
 
 	var options = {};
 	new Chart( pieCtx ).Pie( data, options );
+
+	$( "#categoriesPie" ).parent().append( '<table class="legend">' + html + '</table>' );
 });
