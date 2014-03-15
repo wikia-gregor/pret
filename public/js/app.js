d115c1e92f347f8f5fb86b4c064a7f2595f24099
nAndy <nandy@wikia-inc.com>
2014-03-15 20:10:06 +0100

    replaced mocked data for recent reports with real data

--- public/js/app.js
+++ public/js/app.js
@@ -37,24 +37,28 @@ $( document ).ready( function() {
 		} );
 	} );
 
-	// Getting data
-	// Mocked data
-	var recentReports = [
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "Terrible Hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you." },
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "Terrible, terrible hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "A terrible hole in a road" },
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "Goddamn Hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "My money's in that office, right? If she start giving me some bullshit about it ain't there, and we got to go someplace else and get it, I'm gonna shoot you in the head then and there." },
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "Aweful Hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass." },
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "A Hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me!" },
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "I hate this hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "But I can't give you this case, it don't belong to me." },
-		{ geo_point: { "longitude": 20.981991, "latitude": 52.232951 }, name: "The Hole", category_id: 1, status_id: 1, created: "", file_url: "http://placekitten.com/50/50", description: "Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! " },
-	];
+	// Getting & displaying latest reports
+	Parse.Cloud.run( 'getLatestReports' );
+	Parse.Cloud.run( 'getLatestReports', {}, {
+		success: function( result ) {
+			var recentReports = result.models;
+			var recentReportsWrapper = $( "#recentReports" );
+			var template = '<li><h3>{{name}}</h3><img src="{{file_url}}" /><p>{{{description}}}</p></li><br class="clear" />';
+			var output = '';
 
-	// displaying recent reports
-	var recentReportsWrapper = $( "#recentReports" );
-	var template = '<li><h3>{{name}}</h3><img src="{{file_url}}" /><p>{{{description}}}</p></li><br class="clear" />';
-	var output = '';
-	for( i in recentReports ) {
-		output += Mustache.render( template, recentReports[i] );
-	}
-	recentReportsWrapper.html( output );
+			for( i in recentReports ) {
+				var report = recentReports[i];
+				output += Mustache.render( template, {
+					name: report.get( 'name' ),
+					file_url: report.get( 'file_url' ),
+					description: report.get( 'description' )
+				} );
+			}
+
+			recentReportsWrapper.html( output );
+		},
+		error: function( error ) {
+			console.log( 'ERROR: Could not get latest reports (' + error.message + ')' );
+		}
+	});
 } );
