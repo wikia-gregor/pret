7d7ca2d13e59ccd8aab46ed32e09f028112ec99e
nAndy <nandy@wikia-inc.com>
2014-03-16 12:59:29 +0100

    real data for categories and added number of reports in categories

--- public/js/charts/categories.js
+++ public/js/charts/categories.js
@@ -1,27 +1,47 @@
 $(function() {
-	// mocked data
-	var categoriesNames = [ "Holes", "Signs broken", "Signs missing", "Garbage" ];
-	var categoriesColors = [ "#FF0000", "#E0E4CC", "#69D2E7", "#F38630" ];
-	var categoryReports = [ 40, 15, 5, 22 ];
+	Parse.Cloud.run( 'getReportsPerCategory', {} ).then(
+		function( result ) {
+			render( result );
+		},
+		function( error ) {
+			console.error( 'Could not get categories (' + error.message + ')' );
+		}
+	);
 
-	var pieCtx = $( "#categoriesPie" ).get(0).getContext("2d");
-	var data = [];
-	var l = categoryReports.length;
-	var rowTpl = '<tr><td><span style="background-color: {{color}};"></span></td><td>{{name}}</td></tr>';
-	var html = '';
-	for( var i = 0; i < l; i++ ) {
-		var color = categoriesColors[i];
-		var el = {
-			value: categoryReports[i],
-			color: color
-		};
+	function render( categories ) {
+		var options = {};
+		var data = [];
+		var html = '';
 
-		html += Mustache.render( rowTpl, { 'color': color, name: categoriesNames[i] } );
-		data.push(el);
+		var categoriesPieWrapper = $( "#categoriesPie" );
+		var pieCtx = categoriesPieWrapper.get( 0 ).getContext( "2d" );
+
+		var rowTpl = '<tr><td><span style="background-color: {{color}};"></span></td><td>{{name}} ({{amount}})</td></tr>';
+		var colors = [ '#585656', '#e1e1e1', '#f92531', '#59c39d', '#2d6b6b' ];
+
+		for( name in categories ) {
+			var color = colors.pop();
+			color = ( color !== '' ? color : '#ff0000' );
+			var value = categories[name];
+			var el = {
+				'value': value,
+				'color': color
+			};
+
+			html += Mustache.render( rowTpl, {
+				'color': color,
+				'name': name,
+				'amount': value
+			} );
+
+			data.push( el );
+		}
+
+		new Chart( pieCtx ).Pie( data, options );
+
+		categoriesPieWrapper.parent().append( '<table class="legend">' + html + '</table>' );
+		$( "#categoriesPieLoader" ).hide();
+		$( "#categoriesPieContainer" ).show();
 	}
 
-	var options = {};
-	new Chart( pieCtx ).Pie( data, options );
-
-	$( "#categoriesPie" ).parent().append( '<table class="legend">' + html + '</table>' );
 });
