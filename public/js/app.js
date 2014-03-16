14238e5ebb022df83e8ea51bb43bb0dce9efe9d3
nAndy <nandy@wikia-inc.com>
2014-03-16 13:59:22 +0100

    updated the jquery version, added animation for scrolling

--- public/js/app.js
+++ public/js/app.js
@@ -62,4 +62,15 @@ $( document ).ready( function() {
 			console.error( 'Could not get latest reports (' + error.message + ')' );
 		}
 	});
+
+	$( "nav a" ).click( function( event ) {
+		event.preventDefault();
+		var full_url = this.href;
+		var parts = full_url.split("#");
+		var trgt = parts[1];
+		var target_offset = $("#"+trgt).offset();
+		var target_top = target_offset.top;
+
+		$( "html, body" ).animate( { scrollTop: target_top }, 500 );
+	});
 } );
